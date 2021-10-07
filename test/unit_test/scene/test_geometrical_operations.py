929096e04a1acc02382520ffa639479d3b5b0d95
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-10-07 01:15:12 -0300

    Updated Tests with new changes.
    Updated tests related to the functionality of the method get_max_min_inside_polygon from the class TransformationHelper.

--- test/unit_test/scene/test_geometrical_operations.py
+++ test/unit_test/scene/test_geometrical_operations.py
@@ -23,7 +23,81 @@ import unittest
 
 import numpy as np
 
-from src.engine.scene.geometrical_operations import merge_matrices
+from src.engine.scene.geometrical_operations import get_max_min_inside_polygon, merge_matrices
+
+
+class TestMinMaxPolygon(unittest.TestCase):
+
+    def test_min_max_normal(self):
+
+        # create and set the points on the grid
+        points = np.zeros((10, 10, 3))
+        for row in range(10):
+            for col in range(10):
+                points[row, col, 0] = col
+                points[row, col, 1] = row
+
+        # create the polygon
+        polygon_points = [0.9, 0, 0,
+                          5, 0, 0,
+                          1, 5, 0]
+
+        # create heights of the map
+        height = np.zeros((10, 10))
+        height[1, 1] = -15
+        height[2, 2] = 30
+
+        self.assertEqual((30, -15), get_max_min_inside_polygon(points,
+                                                               polygon_points,
+                                                               height),
+                         'Minimum and maximum values are not the one inside the polygon.')
+
+    def test_min_max_same_value(self):
+
+        # create and set the points on the grid
+        points = np.zeros((10, 10, 3))
+        for row in range(10):
+            for col in range(10):
+                points[row, col, 0] = col
+                points[row, col, 1] = row
+
+        # create the polygon
+        polygon_points = [1, 0, 0,
+                          5, 0, 0,
+                          1, 5, 0]
+
+        # create heights of the map
+        height = np.ones((10, 10))
+        height = height * 150
+
+        self.assertEqual((150, 150), get_max_min_inside_polygon(points,
+                                                                polygon_points,
+                                                                height),
+                         'Minimum and maximum values are not the one inside the polygon.')
+
+    def test_min_max_polygon_outside(self):
+
+        # create and set the points on the grid
+        points = np.zeros((10, 10, 3))
+        for row in range(10):
+            for col in range(10):
+                points[row, col, 0] = col
+                points[row, col, 1] = row
+
+        # create the polygon
+        polygon_points = [100, 0, 0,
+                          150, 0, 0,
+                          125, 50, 0]
+
+        # create heights of the map
+        height = np.ones((10, 10))
+        height = height * 150
+
+        (max_, min_) = get_max_min_inside_polygon(points,
+                                                  polygon_points,
+                                                  height)
+        self.assertEqual((np.nan, np.nan), (max_, min_),
+                         'Minimum and maximum values are not the one inside the polygon.')
 
 
 class TestMergeMatrices(unittest.TestCase):
